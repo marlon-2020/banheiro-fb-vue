3fd9a5e0492426d2e1ecfeff82243793ce334923
marlon-2020 <marlon.ufpe@gmail.com>
2024-06-17 16:35:37 -0300

    aprendendo

---
+++ banheiro/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="clica">
+        clica
+    </button>
+</template>
+<script>
+    export default {
+        data() {
+           return {
+                cumprimento: 'olá, eu sou o Goku!'
+           }
+        },
+        methods: {
+            clica(){
+                console.log(this.cumprimento)
+            }
+        }
+    }
+</script>
