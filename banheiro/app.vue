2d964b06883cacff53795324e913d88943acaa9f
marlon-2020 <marlon.ufpe@gmail.com>
2024-06-19 13:00:32 -0300

    adiciona barra de navegação

--- banheiro/app.vue
+++ banheiro/app.vue
@@ -3,3 +3,13 @@
     <Header/>
   </div>
 </template>
+<style>
+  *{
+    margin: 0;
+    box-sizing: border-box;
+    padding: 0;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.3rem;
+    text-decoration: none;
+  }
+</style>
--- banheiro/components/Header.vue
+++ banheiro/components/Header.vue
@@ -1,19 +1,46 @@
 <template>
-    <button @click="clica">
-        clica
-    </button>
+    <header>
+        <nav>
+            <a href="" class="link">Fila de espera</a>
+            <a href="" class="link">Já foram</a>
+        </nav>
+    </header>
 </template>
 <script>
     export default {
         data() {
            return {
-                cumprimento: 'olá, eu sou o Goku!'
+                cumprimento: ''
            }
         },
         methods: {
-            clica(){
-                console.log(this.cumprimento)
-            }
+            
         }
     }
 </script>
+<style>
+    nav{
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: rgb(125,0,150);
+    }
+    a.link{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 50%;
+        height: 100%;
+        text-align: center;
+        color: rgb(240,240,240);
+        transition: background-color 0.5s;
+    }
+    a.link:nth-of-type(odd){
+        border-right: 1px solid white;
+        border-collapse: collapse;
+    }
+    a.link:hover{
+        background-color: rgb(200, 50, 50);
+    }
+</style>
